2fab1c4f2d967edfce5224ef2a6a5f5ff73b1be9
Ali <ali.shakoor2249@gmail.com>
2022-11-18 15:16:17 -0500

    Added ViewModel Class and updated .csproj file

---
+++ TaskandLog/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SQLite;
+
+namespace TaskandLog.ViewModel;
+
+	public partial class MainPageViewModel
+{
+
+}
+	
